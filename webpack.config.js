7f7bbb4a59817c0215936a6cce86cdd5dddd0224
shinework <adrien.baptiste@gmail.com>
2015-12-02 14:51:44 +0100

    Update babel version and other dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 'use strict';
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require("webpack");
-var path = require("path");
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  target: "web",
+  target: 'web',
   cache: true,
   context: __dirname,
   entry: {
-    app: path.join(__dirname, "src/app"),
+    app: path.join(__dirname, 'src/app'),
     vendor: ['react', 'react-dom', 'react-router', 'react-mixin', 'alt', 'history']
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
     publicPath: '/'
   },
   module: {
     loaders: [
       {
-        include: /\.js$/, loaders: ["react-hot", "babel-loader?stage=0"], exclude: /node_modules/
+        include: /\.js$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/
       },
       {
         test: /\.css$/,
@@ -40,6 +40,6 @@ module.exports = {
   ],
   node: {
     __dirname: true,
-    fs: "empty"
+    fs: 'empty'
   }
 };
